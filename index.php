621ba6654c9ee01567cd501c0579eafac2c0a106
Samuel Xavier <sancrisxa@gmail.com>
2019-09-18 18:43:45 -0300

    Finalizacao do projeto

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<table border="0" width="100%">
+    <tr>
+        <th>Imagem</th>
+        <th>Titulo</th>
+        <th>Autor</th>
+        <th>Views</th>
+        <th>Tempo</th>
+        <th>Link</th>
+    </tr>
+
+
 <?php
 
 require 'simple_html_dom.php';
@@ -8,8 +19,41 @@ $results = $html->find('.yt-lockup');
 
 echo "TOTAL DE RESULTADOS :" .  count($results);
 
-foreach ($results as $videos) {
-    $imagem = $videos->find('.yt-thumb-simple img', 0);
+foreach ($results as $video) {
+    $imagem = $video->find('.yt-thumb-simple img', 0)->attr['src'] ?? '';
+    $tempo = $video->find('.video-time', 0)->plaintext ?? '';
+    $link = $video->find('.yt-lockup-title a', 0)->href ?? '';
+    $titulo = $video->find('.yt-lockup-title a', 0)->plaintext ?? '';
+    $autor = $video->find('.yt-lockup-byline a', 0)->plaintext ?? '';
+    $views = $video->find('.yt-lockup-meta-info li', 1)->plaintext ?? '';
 
-    print_r($imagem);
+?>
+
+    <tr>
+        <td>
+            <img src="<?php echo $imagem;?>" alt="">
+        </td>
+        <td>
+            <?php echo $titulo;?>
+        </td>
+        <td>
+            <?php echo $autor;?>
+        </td>
+        <td>
+            <?php echo $views;?>
+        </td>
+        <td>
+            <?php echo $tempo;?>
+        </td>
+        <td>
+            <?php echo $link;?>
+        </td>
+    </tr>
+
+<?php
+
 }
+
+?>
+</table>
+
